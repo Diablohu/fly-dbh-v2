484dd8031aabf5b5c8e431605fc449b1f7343427
Diablohu <huxin@cmcm.com>
2025-06-18 17:27:44 +0800

    try resolving image proxy

--- src/services/sanity.ts
+++ src/services/sanity.ts
@@ -1,13 +1,8 @@
 import { createClient, type SanityDocument } from "@sanity/client";
-
 import cache from "./_cache";
 
 // ============================================================================
 
-console.log({
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
-});
 export const client = createClient({
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: process.env.SANITY_DATASET,
