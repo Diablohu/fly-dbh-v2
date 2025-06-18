1caff39f972f352eaca9ccbd3ce6116c60f261cf
Diablohu <huxin@cmcm.com>
2025-06-18 17:59:27 +0800

    test

--- src/pages/sanity-images/[imagePath].ts
+++ src/pages/sanity-images/[imagePath].ts
@@ -2,11 +2,12 @@ import { type APIRoute } from "astro";
 
 const target = `https://cdn.sanity.io/images/${process.env.SANITY_PROJECT_ID}/${process.env.SANITY_DATASET}`;
 
-export const ALL: APIRoute = async ({ request, params, url }) => {
+export const ALL: APIRoute = async ({ params, url }) => {
     const fullUrl = `${target}/${params.imagePath}${url.search}`;
     // console.log({ fullUrl });
     try {
-        return await fetch(fullUrl, request);
+        return fetch(fullUrl);
+        // return (await axios.get(fullUrl)).data;
     } catch (e) {
         console.log(e);
         console.trace(e);
