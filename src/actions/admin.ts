47ce2a669c705eb08f952ecabb55d2d7db1baf0c
Diablohu <Diablohu@users.noreply.github.com>
2025-07-09 05:22:44 +0800

    feature: `Layout` supports side bar content

--- src/actions/admin.ts
+++ src/actions/admin.ts
@@ -1,4 +1,8 @@
-import { defineAction, ActionError } from "astro:actions";
+import {
+    defineAction,
+    ActionError,
+    type ActionAPIContext,
+} from "astro:actions";
 import { z } from "astro:schema";
 import { createTOTPKeyURI, verifyTOTPWithGracePeriod } from "@oslojs/otp";
 import dayjs from "dayjs";
@@ -12,6 +16,26 @@ import actionErrorHandler from "./_error-handler";
 
 const issuer = title;
 const accountName = "Diablohu";
+
+// ============================================================================
+
+function isLoginValid(context: ActionAPIContext) {
+    const lastLoggedIn = context.cookies.get(ADMIN_LAST_LOGIN)?.value;
+    return (
+        lastLoggedIn &&
+        Date.now() - Number(lastLoggedIn) < adminLoginValidPeriod
+    );
+}
+function refreshCookie(context: ActionAPIContext) {
+    const now = Date.now();
+    const expiresTimestamp = now + adminLoginValidPeriod;
+    context.cookies.set(ADMIN_LAST_LOGIN, now.toString(), {
+        path: "/admin",
+        expires: dayjs(expiresTimestamp).toDate(),
+    });
+
+    return expiresTimestamp;
+}
 
 // ============================================================================
 
@@ -50,14 +74,27 @@ const actions = {
 
             if (!valid) throw new ActionError({ code: "UNAUTHORIZED" });
 
-            const now = Date.now();
-            const expiresTimestamp = now + adminLoginValidPeriod;
-            context.cookies.set(ADMIN_LAST_LOGIN, now.toString(), {
-                path: "/admin",
-                expires: dayjs(expiresTimestamp).toDate(),
-            });
             return {
-                expires: expiresTimestamp,
+                expires: refreshCookie(context),
+            };
+        },
+    }),
+
+    adminIsLoginValid: defineAction({
+        handler: async (_, context) => {
+            if (isLoginValid(context)) return true;
+
+            context.cookies.delete(ADMIN_LAST_LOGIN);
+            return false;
+        },
+    }),
+
+    adminRefreshLoginCookie: defineAction({
+        handler: async (_, context) => {
+            if (!isLoginValid(context))
+                throw new ActionError({ code: "UNAUTHORIZED" });
+            return {
+                expires: refreshCookie(context),
             };
         },
     }),
