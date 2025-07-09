e7bda9efa4be2724605ab9065159d2ecc2d62bdb
Diablohu <Diablohu@users.noreply.github.com>
2025-07-09 11:07:09 +0800

    polish: [admin] login action

--- src/page-templates/admin/login-form.tsx
+++ src/page-templates/admin/login-form.tsx
@@ -1,16 +1,10 @@
-import {
-    useState,
-    useCallback,
-    useEffect,
-    useRef,
-    type FC,
-    type FormEventHandler,
-} from "react";
-import qrcode from "qrcode";
+import { useState, useCallback, type FC, type FormEventHandler } from "react";
+import { actions } from "astro:actions";
 
 // ============================================================================
 
 type StatusType = "ready" | "loading" | "error";
+const loginAction = actions.adminLogin;
 
 // ============================================================================
 
@@ -24,25 +18,33 @@ const LoginForm: FC = () => {
 
             if (status === "loading") return;
 
-            const action = evt.currentTarget.getAttribute("action");
-            const method = evt.currentTarget.getAttribute("method");
-            if (!action) return;
-            if (!method) return;
+            // const action = evt.currentTarget.getAttribute("action");
+            // const method = evt.currentTarget.getAttribute("method");
+            // if (!action) return;
+            // if (!method) return;
 
             setError("");
             setStatus("loading");
-            const res = await fetch(action, {
-                method: method.toUpperCase(),
-                body: new FormData(evt.currentTarget),
-            });
+            // const res = await fetch(action, {
+            //     method: method.toUpperCase(),
+            //     body: new FormData(evt.currentTarget),
+            // });
+            const { data, error } = await loginAction(
+                new FormData(evt.currentTarget)
+            );
 
-            if (res.status !== 200) {
+            // if (res.status !== 200) {
+            //     setStatus("error");
+            //     setError(`Error: ${res.status} ${res.statusText}`);
+            //     return;
+            // }
+            if (!data || error) {
                 setStatus("error");
-                setError(`Error: ${res.status} ${res.statusText}`);
+                setError(`${error.status} ${error.code}`);
                 return;
             }
 
-            const data = await res.json();
+            // const data = await res.json();
 
             if (typeof data.expires === "number") window.location.reload();
 
@@ -53,7 +55,7 @@ const LoginForm: FC = () => {
 
     return (
         <>
-            <form action="/admin/login" method="post" onSubmit={onSubmit}>
+            <form action={loginAction} method="POST" onSubmit={onSubmit}>
                 <input type="text" name="code" autoComplete="off" required />
                 <input type="submit" value="GO" />
             </form>
