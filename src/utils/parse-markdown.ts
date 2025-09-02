9a3b1120ec0c2cae8cf07512244afb117fef2228
Diablohu <Diablohu@users.noreply.github.com>
2025-09-03 00:59:47 +0800

    polish: [watch page] description & links

---
+++ src/utils/parse-markdown.ts
@@ -0,0 +1,15 @@
+import { marked } from "marked";
+import DOMPurify from "isomorphic-dompurify";
+
+// ============================================================================
+
+function parseMarkdown(src: string) {
+    return DOMPurify.sanitize(
+        marked.parse(
+            src.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""),
+            { async: false, breaks: true, gfm: true }
+        )
+    );
+}
+
+export default parseMarkdown;
