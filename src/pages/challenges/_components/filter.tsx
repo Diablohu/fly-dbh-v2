b78a32e5864f16eb4279f8404d5245fb965b6f1e
Diablohu <Diablohu@users.noreply.github.com>
2025-09-13 20:25:56 +0800

    feature: [challenges] WIP

--- src/pages/challenges/_components/filter.tsx
+++ src/pages/challenges/_components/filter.tsx
@@ -1,6 +1,14 @@
 /*
 
+TODO:
+
+by aircraft category
+    A, B, C, D
+
+by difficulty
     <Block class="list">小有挑战</Block>
     <Block class="list">相当挑战</Block>
     <Block class="list">极具挑战</Block>
+
+by hazard
  */
