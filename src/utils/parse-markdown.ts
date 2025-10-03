60f5450da919f551ee2e099171f17af4299d6a6c
Diablohu <Diablohu@users.noreply.github.com>
2025-10-04 02:10:41 +0800

    fix: `parseMarkdown()`

--- src/utils/parse-markdown.ts
+++ src/utils/parse-markdown.ts
@@ -4,6 +4,7 @@ import DOMPurify from "isomorphic-dompurify";
 // ============================================================================
 
 function parseMarkdown(src: string) {
+    if (!src) return src;
     return DOMPurify.sanitize(
         marked.parse(
             src.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""),
