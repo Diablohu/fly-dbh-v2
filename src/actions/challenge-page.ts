88855ca5e3c8ca3c50e417e3bf781b04e31a8212
Diablohu <Diablohu@users.noreply.github.com>
2025-11-21 03:28:26 +0800

    feature: [challenges] new action to fetch latest

--- src/actions/challenge-page.ts
+++ src/actions/challenge-page.ts
@@ -191,6 +191,58 @@ const actions = {
             }
         },
     }),
+
+    fetchLatest: defineAction({
+        input: z.object({
+            length: z.number().optional(),
+        }),
+        handler: async ({ length = 10 }) => {
+            try {
+                const queryString = `*[_type == "approach_challenge"] {
+  _id,
+  'slug': slug.current,
+  'aerodrome': aerodrome->{
+    _id,
+    'slug': slug.current,
+    name,
+    icao,
+    iata,
+    faa,
+    location,
+  },
+  name,
+  difficulty,
+  max_allowed_aircraft_category
+} | order( _updateAt desc ) [0...${length}]`;
+                const res = await fetch<ChallengeListItemType>(queryString, {
+                    transform: (res, queryString) => {
+                        if (!res[0]) {
+                            const err = new ActionError({
+                                message: E60000,
+                                code: "NOT_FOUND",
+                            });
+                            err.cause = { GROQ: queryString };
+                            throw err;
+                        }
+                        // res[0].cover = transformImagePath(res[0].cover);
+                        return res;
+                    },
+                });
+
+                if (!res) {
+                    const err = new ActionError({
+                        message: E60000,
+                        code: "NOT_FOUND",
+                    });
+                    err.cause = { GROQ: queryString };
+                    throw err;
+                }
+                return res;
+            } catch (err) {
+                actionErrorHandler(err);
+            }
+        },
+    }),
 };
 
 export default actions;
