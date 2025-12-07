408d3650876eb49d9dad9a63dfffe62e7be71900
Diablohu <Diablohu@users.noreply.github.com>
2025-12-08 05:27:25 +0800

    util: [markdown] force webp format for png file

--- src/utils/parse-markdown.ts
+++ src/utils/parse-markdown.ts
@@ -6,15 +6,33 @@ import DOMPurify from "isomorphic-dompurify";
 function parseMarkdown(src: string) {
     if (!src) return src;
     return DOMPurify.sanitize(
-        marked.parse(
-            src.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""),
-            { async: false, breaks: true, gfm: true }
-        )
+        marked
+            .use({
+                renderer: {
+                    image(token) {
+                        const url = new URL(token.href, "https://fly-dbh.com");
+                        if (
+                            /\.png$/.test(url.pathname) &&
+                            url.searchParams.get("auto") === "format"
+                        ) {
+                            url.searchParams.delete("auto");
+                            url.searchParams.set("fm", "webp");
+                            url.searchParams.set("q", "80");
+                        }
+                        // console.log(token, url.pathname + url.search);
+                        /*
+                        TODO: Image Viewer
+                        */
+                        return `<img src="${url.pathname + url.search}" alt="${token.text}" />`;
+                    },
+                },
+            })
+            .parse(src.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""), {
+                async: false,
+                breaks: true,
+                gfm: true,
+            })
     );
 }
 
 export default parseMarkdown;
-
-/*
-TODO: Image Viewer
-*/
