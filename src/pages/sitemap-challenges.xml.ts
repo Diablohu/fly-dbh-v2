2347c9b34f0bc42126a7a7670e5fa301cc11cf04
Diablohu <Diablohu@users.noreply.github.com>
2025-12-19 02:18:47 +0800

    fix: [sitemap] make sure `videos` & `challenges` are XML response

--- src/pages/sitemap-challenges.xml.ts
+++ src/pages/sitemap-challenges.xml.ts
@@ -3,22 +3,19 @@ import { actions } from "astro:actions";
 import dayjs from "dayjs";
 import minMax from "dayjs/plugin/minMax";
 import getChallengePageLink from "@/utils/get-challenge-page-link";
+import { generateResponse } from "@/services/sitemap";
 
 dayjs.extend(minMax);
 
 export const GET: APIRoute = async ({ site, callAction }) => {
-    return new Response(`\
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-${(await callAction(actions.sitemap.fetchChallenges, undefined)).data
-    ?.map(
-        (item) => `\
-    <url>
-        <loc>${site}${getChallengePageLink(item.slug || item._id).slice(1)}</loc>
-        <lastmod>${dayjs
-            .max(dayjs(item._updatedAt), dayjs(item.aerodrome._updatedAt))
-            .format("YYYY-MM-DD")}</lastmod>
-    </url>`
-    )
-    .join("\n")}
-</urlset>`);
+    return generateResponse(
+        (
+            await callAction(actions.sitemap.fetchChallenges, undefined)
+        ).data?.map((item) => ({
+            loc: `${site}${getChallengePageLink(item.slug || item._id).slice(1)}`,
+            lastmod: `${dayjs
+                .max(dayjs(item._updatedAt), dayjs(item.aerodrome._updatedAt))
+                .format("YYYY-MM-DD")}`,
+        })) || []
+    );
 };
--- src/pages/sitemap-videos.xml.ts
+++ src/pages/sitemap-videos.xml.ts
@@ -2,18 +2,15 @@ import type { APIRoute } from "astro";
 import { actions } from "astro:actions";
 import dayjs from "dayjs";
 import { getVideoPageLink } from "@/global";
+import { generateResponse } from "@/services/sitemap";
 
 export const GET: APIRoute = async ({ site, callAction }) => {
-    return new Response(`\
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-${(await callAction(actions.sitemap.fetchVideos, undefined)).data
-    ?.map(
-        (item) => `\
-    <url>
-        <loc>${site}${getVideoPageLink(item.slug || item._id).slice(1)}</loc>
-        <lastmod>${dayjs(item._updatedAt).format("YYYY-MM-DD")}</lastmod>
-    </url>`
-    )
-    .join("\n")}
-</urlset>`);
+    return generateResponse(
+        (await callAction(actions.sitemap.fetchVideos, undefined)).data?.map(
+            (item) => ({
+                loc: `${site}${getVideoPageLink(item.slug || item._id).slice(1)}`,
+                lastmod: `${dayjs(item._updatedAt).format("YYYY-MM-DD")}`,
+            })
+        ) || []
+    );
 };
