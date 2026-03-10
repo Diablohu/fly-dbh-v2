84e2e54565e6952f521d5a0b0da9a68a9b369e1b
Diablohu <Diablohu@users.noreply.github.com>
2026-03-11 04:30:06 +0800

    update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,15 +2,15 @@ import type { APIRoute } from "astro";
 
 const getRobotsTxt = (site: URL) => `\
 User-agent: *
-${(import.meta.env.FLYDBH_BUILD_MODE === "next"
-    ? ["/"]
-    : ["/includes/", "/admin/"]
-)
-    .map((path) => `Disallow: ${path}`)
-    .join("\n")}
+${
+    import.meta.env.FLYDBH_BUILD_MODE === "next"
+        ? "Disallow: /"
+        : `${["/includes/", "/admin/"]
+              .map((path) => `Disallow: ${path}`)
+              .join("\n")}
 
-Sitemap: ${new URL("sitemap-index.xml", site).href}
-`;
+Sitemap: ${new URL("sitemap-index.xml", site).href}`
+}`;
 
 export const GET: APIRoute = ({ site }) => {
     if (!site) return new Response("Site URL is not defined", { status: 500 });
