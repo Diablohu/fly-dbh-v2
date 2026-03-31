64f7326d41ba36730bc57596f03a45b4d2c4f70f
Diablohu <Diablohu@users.noreply.github.com>
2026-04-01 04:10:52 +0800

    new react-hook: `useSticky`

--- src/pages/challenges/_components-details-page/title.tsx
+++ src/pages/challenges/_components-details-page/title.tsx
@@ -1,8 +1,9 @@
-import { useRef, useEffect, useState, type FC } from "react";
+import { useRef, type FC } from "react";
 import classNames from "classnames";
 
 import { type ChallengeItemType } from "@/types";
 import { routeBase } from "@/global";
+import useSticky from "@/react-hooks/use-sticky";
 import leftArrow from "@/assets/arrow/left3.svg?raw";
 
 import styles from "./title.module.less";
@@ -14,37 +15,8 @@ const TitleBlock: FC<{
     challengeName: string;
     airacCycle?: ChallengeItemType["airac_cyle"];
 }> = ({ aerodromeName, challengeName, airacCycle }) => {
-    const ProbeRef = useRef<HTMLSpanElement>(null);
-    const ProbeObserverRef = useRef<IntersectionObserver>(null);
-    const [isSticky, setSticky] = useState(false);
-
-    useEffect(() => {
-        if (!ProbeObserverRef.current)
-            ProbeObserverRef.current = new IntersectionObserver(
-                (entries) => {
-                    entries.forEach((entry) => {
-                        if (entry.isIntersecting) {
-                            // console.log("inview");
-                            setSticky(false);
-                        } else {
-                            // console.log("not inview");
-                            setSticky(true);
-                        }
-                    });
-                },
-                { threshold: 0 }
-            );
-
-        if (ProbeRef.current)
-            ProbeObserverRef.current?.observe(ProbeRef.current);
-
-        return () => {
-            if (ProbeObserverRef.current && ProbeRef.current) {
-                ProbeObserverRef.current.unobserve(ProbeRef.current);
-            }
-            ProbeObserverRef.current?.disconnect();
-        };
-    }, []);
+    const ContainerRef = useRef<HTMLElement>(null);
+    const { isSticky } = useSticky({ ContainerRef });
 
     return (
         <section
@@ -54,6 +26,7 @@ const TitleBlock: FC<{
                     [styles["is-sticky"]]: isSticky,
                 },
             ])}
+            ref={ContainerRef}
         >
             <a
                 className={styles["link-to-challenge-list"]}
@@ -70,7 +43,6 @@ const TitleBlock: FC<{
                     {challengeName}
                 </small>
             </h1>
-            <em className={styles["intersection-probe"]} ref={ProbeRef} />
             {airacCycle && (
                 <span className={styles["airac-cycle"]}>
                     本文基于 AIRAC CYCLE <strong>{airacCycle}</strong>
---
+++ src/react-hooks/use-sticky.ts
@@ -0,0 +1,74 @@
+// TODO: https://caniuse.com/?search=scroll-state
+
+import { useEffect, useRef, useState, type RefObject } from "react";
+
+const useSticky = (
+    opt:
+        | {
+              ProbeRef: RefObject<HTMLElement | null>;
+          }
+        | {
+              ContainerRef: RefObject<HTMLElement | null>;
+              cssVariableNameExtraTop?: string;
+          },
+) => {
+    const ProbeRef = useRef<HTMLElement>(null);
+    const ProbeObserverRef = useRef<IntersectionObserver>(null);
+    const [isSticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        // 如果没有提供 `ProbeRef`，新建一个元素作为探测目标
+        if (
+            !("ProbeRef" in opt) &&
+            "ContainerRef" in opt &&
+            !ProbeRef.current
+        ) {
+            ProbeRef.current = document.createElement("em");
+            ProbeRef.current.style.position = "absolute";
+            ProbeRef.current.style.zIndex = "-100";
+            ProbeRef.current.style.top = `calc(var(--global-sticky-top) * -1${
+                opt.cssVariableNameExtraTop
+                    ? ` - var(${opt.cssVariableNameExtraTop})`
+                    : ""
+            })`;
+            ProbeRef.current.style.left = "0";
+            ProbeRef.current.style.width = "0";
+            ProbeRef.current.style.height = "0";
+            ProbeRef.current.setAttribute("aria-hidden", "true");
+            ProbeRef.current.setAttribute("data-role", "sticky-probe");
+            opt.ContainerRef.current?.appendChild(ProbeRef.current);
+        }
+
+        const probeElement =
+            "ProbeRef" in opt ? opt.ProbeRef.current : ProbeRef.current;
+
+        if (!ProbeObserverRef.current)
+            ProbeObserverRef.current = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            // console.log("inview");
+                            setSticky(false);
+                        } else {
+                            // console.log("not inview");
+                            setSticky(true);
+                        }
+                    });
+                },
+                { threshold: 0 },
+            );
+
+        if (probeElement) ProbeObserverRef.current?.observe(probeElement);
+
+        return () => {
+            if (ProbeObserverRef.current && probeElement) {
+                ProbeObserverRef.current.unobserve(probeElement);
+            }
+            ProbeObserverRef.current?.disconnect();
+        };
+    }, []);
+
+    return { isSticky };
+};
+
+export default useSticky;
