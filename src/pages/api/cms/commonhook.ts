bca6017e57ec2b2943162c244c564312398d0bbf
Diablohu <Diablohu@users.noreply.github.com>
2026-06-23 17:54:43 +0800

    update log

--- src/pages/api/cms/commonhook.ts
+++ src/pages/api/cms/commonhook.ts
@@ -4,9 +4,12 @@ import type { APIRoute } from "astro";
  * 接收 Sanity CMS 发来的 webhook
  *  - 任何文档在 create、update 和 delete 时都会发送该 webhook
  */
-export const POST: APIRoute = async ({ request, cache }) => {
+export const POST: APIRoute = async ({
+    request,
+    // cache
+}) => {
     // const data = await request.formData();
-    console.log({ request, cache });
+    console.log("CMS Webhook", request.body);
     // const { slug } = await request.json();
 
     // // Invalidate every response tagged 'products'...
